b9bf5ab8cdde36a60abb9da27de4e30bc10f6f28
shiyibode <1208874645@qq.com>
2023-02-23 11:05:12 +0800

    Signed-off-by: shiyibode <1208874645@qq.com>

--- app.js
+++ app.js
@@ -5,15 +5,37 @@ App({
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
+    var app = this
 
     // 登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        
+        console.log(res.code)
+
+        wx.request({
+          url: 'http://192.168.2.225:8080/code',
+          data:{code:res.code},
+          success(res){
+            // resData = res;
+            app.globalData.openId=res.data ;
+            console.log(res.data);
+          }
+        })
+
+
       }
     })
+
+
+
+    //企业微信登录
+    
   },
+
   globalData: {
+    openId: null,
     userInfo: null
   }
 })
---
+++ pages/jixiao/jixiao.js
@@ -0,0 +1,77 @@
+// pages/jixiao/jixiao.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    resData: null
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+    var appInstance = getApp()
+    console.log(appInstance.globalData) // I am global data
+
+    wx.request({
+      url: 'http://192.168.2.225:8080/info', 
+      success(res){
+        // resData = res;
+        console.log(res);
+      }
+    })
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
